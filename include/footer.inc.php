959bb73461eec8be0bb20e9c8f472981314882d2
Imene <khelilimene03@gmail.com>
2025-04-18 22:28:44 +0200

    restauration des cookies

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -14,7 +14,27 @@
         <span>CYU TECH CERGY</span>
         <span><a href="tech.php">Page tech</a></span>
         <span><?php echo date("d/m/Y");?></span>
+		<div id="cookie-banner" class="cookie-banner" style="display: none;">
+    		<p>Ce site utilise des cookies pour améliorer l’expérience utilisateur.</p>
+    		<button id="accept-cookies">Accepter</button>
+		</div>
+
     </footer>
     </main>
+	<script>
+	document.addEventListener("DOMContentLoaded", () => {
+    	const banner = document.getElementById("cookie-banner");
+    	const acceptBtn = document.getElementById("accept-cookies");
+
+    	if (!localStorage.getItem("cookiesAccepted")) {
+        	banner.style.display = "flex";
+    	}
+
+    	acceptBtn.addEventListener("click", () => {
+        	localStorage.setItem("cookiesAccepted", "yes");
+        	banner.style.display = "none";
+    	});
+	});
+	</script>
     </body>
 </html>
