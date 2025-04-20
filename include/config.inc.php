080e377cfdc409f5254524772a4663ea2f234f3d
mathis <mathis.albrun@gmail.com>
2025-04-20 22:25:16 +0200

    modif config

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('WEATHERAPI_KEY_PRO1', '2ab485cff9d84e0f81c112651251504');
-define('WEATHERAPI_KEY_PRO2', 'ta_clé_free');
+define('WEATHERAPI_KEY_PRO2', '0eab3471e1c6478889e202220252004');
 define('WEATHERAPI_PRO_START', '2025-04-28'); // date d’activation de la clé pro
 
 define('NASA_API_KEY', 'rdRhkkOSfDCrBMdSOCnjfHX2XaUAu5O49PX58ALO');
